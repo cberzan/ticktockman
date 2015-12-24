8f96c420fe964e8861142d58d37063a20284e9b4
Constantin Berzan <cberzan@gmail.com>
2015-12-23 19:09:48 -0800

    report filename, lineno for parse errors, not just lineno

--- tools/events.py
+++ tools/events.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from datetime import timedelta
+from functools import partial
 import csv
 import dateutil.parser
 
 # TODO:
-# - errors need filename, not just lineno
 # - regression test: time(0, 0) evaluates as False, causing parsing error
 
 
@@ -28,6 +28,12 @@ class Event(object):
         return self.end - self.begin
 
 
+class ParseError(Exception):
+    def __init__(self, csvfile, reader, message):
+        super(ParseError, self).__init__(
+            "{}:{}: {}".format(csvfile.name, reader.line_num, message))
+
+
 def parse_events(csvfile):
     """
     Read list of events from the given CSV file.
@@ -42,52 +48,46 @@ def parse_events(csvfile):
     cover a contiguous stretch of time, and do not overlap.
     """
     reader = csv.reader(csvfile)
+    error = partial(ParseError, csvfile, reader)
 
     # Read header.
     header = reader.next()
     ref_header = ['date', 'date_comment', 'end_time', 'category', 'comment']
     if header != ref_header:
-        raise ValueError(
-            "Unexpected header line: expected {} but found {}".format(
-                ref_header, header))
+        raise error("Unexpected header line: expected {} but found {}".format(
+            ref_header, header))
 
     # Read first row.
-    prev_row = parse_csv_line(2, reader.next())
+    prev_row = fields_to_raw_line(reader.next(), error)
     if not prev_row.date:
-        raise ValueError("first row must specify date")
+        raise error("First row must specify date")
     if prev_row.end_time is None:
-        raise ValueError("first row must specify end_time")
+        raise error("first row must specify end_time")
     prev_event_end = datetime.combine(prev_row.date, prev_row.end_time)
 
     # Read subsequent rows.
     events = []
     one_day = timedelta(days=1)
     for lineno, fields in enumerate(reader, 3):
-        row = parse_csv_line(lineno, fields)
+        row = fields_to_raw_line(fields, error)
 
         # Date should appear iff time crosses midnight.
         # If date doesn't appear, fill it in from the prev_row.
         if row.date:
             if row.date != prev_row.date + one_day:
-                raise ValueError(
-                    "line {}: expected the day after, but got a different date"
-                    .format(lineno))
+                raise error("Expected the day after, but got a different date")
             if row.end_time >= prev_row.end_time:
-                raise ValueError(
-                    "line {}: got date but did not expect one"
-                    .format(lineno))
+                raise error("Got date but did not expect one")
         else:
             if row.end_time <= prev_row.end_time:
-                raise ValueError(
-                    "line {}: expected date, but did not get one"
-                    .format(lineno))
+                raise error("Expected date, but did not get one")
             row.date = prev_row.date
 
         # End time and category should always appear.
         if row.end_time is None:
-            raise ValueError("line {}: expected end_time".format(lineno))
+            raise error("Expected end_time")
         if not row.category:
-            raise ValueError("line {}: expected category".format(lineno))
+            raise error("Expected category")
 
         # Assemble event.
         event = Event()
@@ -103,23 +103,20 @@ def parse_events(csvfile):
     return events
 
 
-def parse_csv_line(lineno, fields):
+def fields_to_raw_line(fields, error):
     """
-    Parse a CSV line (`fields` is a list of string) into a RawLine object.
+    Parse a list of strings (`fields`) into a RawLine object.
     """
-    # print "parsing line {}: {}".format(lineno, fields)
-
     line = RawLine()
 
     if len(fields) != 5:
-        raise ValueError("line {}: expected {} fields, but found {}".format(
-            lineno, 5, len(fields)))
+        raise error("Expected {} fields, but found {}".format(5, len(fields)))
 
     if fields[0]:
         try:
             line.date = dateutil.parser.parse(fields[0]).date()
         except ValueError:
-            raise ValueError("line {}: could not parse date".format(lineno))
+            raise error("Could not parse date")
 
     if fields[1]:
         line.date_comment = fields[1]
@@ -128,7 +125,7 @@ def parse_csv_line(lineno, fields):
         try:
             line.end_time = dateutil.parser.parse(fields[2]).time()
         except ValueError:
-            raise ValueError("line {}: could not parse time".format(lineno))
+            raise error("Could not parse time")
 
     if fields[3]:
         line.category = fields[3]
